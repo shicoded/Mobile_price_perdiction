7b1f0b5d61d1f5ef52fe32252080c2b41c33a273
Shima Soleymanipour <155065873+shicoded@users.noreply.github.com>
2024-07-21 02:15:55 +0330

    Update code.py

--- code.py
+++ code.py
@@ -15,6 +15,6 @@ from sklearn.model_selection import train_test_split
 # Load the CSV file
 data = pd.read_csv('/content/drive/MyDrive/dataset_folder/mobile data/mobile phone price prediction.csv')
 
-# Display the first few rows of the dataframe to understand its structure
+## Display the first few rows of the dataframe to understand its structure
 
 print(tabulate(data.head(10), headers='keys', tablefmt='psql'))
